7155f425e343ed43fe327a0f88c46ee1e35f040c
Chikal Fachdiana <62524948+ChikalF@users.noreply.github.com>
2021-09-27 14:45:42 +0700

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld
+{
+	public static void main(String[] args) 
+	{
+		int NIM = 6706213056;
+		string NAMA = "CHIKAL FACHDIANA";
+		float IPK = 3.50;
+		
+		System.out.println(NIM);
+		System.out.println(NAMA);
+		System.out.println(IPK);
+	}
+}
---
+++ Mahasiswa.java
@@ -0,0 +1,16 @@
+public class Mahasiswa
+{
+	public static void main(String[] args) 
+	{
+		String NAMA;
+		NAMA = "CHIKAL FACHDIANA";
+		long NIM;
+		NIM = 6706213056L;
+		float IPK;
+		IPK = 3.89f;
+		
+		System.out.println("NAMA: " + NAMA);
+		System.out.println("NIM: " + NIM);
+		System.out.println("IPK: " + IPK);
+	}
+}
